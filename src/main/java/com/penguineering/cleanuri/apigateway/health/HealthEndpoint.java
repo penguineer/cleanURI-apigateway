6b357d73e3cc1741b40d5eba391deda1bfe5e7ea
Stefan Haun <mail@tuxathome.de>
2022-07-20 03:02:33 +0200

    Add Health endpoint
    as implemented in CalMixer

---
+++ src/main/java/com/penguineering/cleanuri/apigateway/health/HealthEndpoint.java
@@ -0,0 +1,42 @@
+package com.penguineering.cleanuri.apigateway.health;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.inject.Inject;
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller("/health")
+public class HealthEndpoint {
+    @Inject
+    List<HealthProvider> providers;
+
+    @Get
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiResponse(responseCode = "200", description = "Service is healthy")
+    @ApiResponse(responseCode = "500", description = "Service is failing")
+    public HttpResponse<String> health() {
+        final JSONObject response = new JSONObject();
+
+        boolean healthy = true;
+
+        for (HealthProvider provider: providers) {
+            Optional<Object> status = provider.getStatus();
+            status.ifPresent(o -> response.put(provider.getKey(), o));
+            healthy &= provider.isHealthy();
+        }
+
+        response.put("healthy", healthy);
+
+        return HttpResponse
+                .status(healthy ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR, null)
+                .body(response.toString(2));
+    }
+}
---
+++ src/main/java/com/penguineering/cleanuri/apigateway/health/VersionHealthProvider.java
@@ -0,0 +1,29 @@
+package com.penguineering.cleanuri.apigateway.health;
+
+import com.penguineering.cleanuri.apigateway.Application;
+import jakarta.inject.Singleton;
+
+import java.util.Optional;
+
+@Singleton
+public class VersionHealthProvider implements HealthProvider {
+    private final String version = Application.class.getPackage().getImplementationVersion();
+
+    @Override
+    public String getKey() {
+        return "version";
+    }
+
+    @Override
+    public Optional<Object> getStatus() {
+        if (this.version == null)
+           return Optional.empty();
+
+        return Optional.of(version);
+    }
+
+    @Override
+    public boolean isHealthy() {
+        return true;
+    }
+}
