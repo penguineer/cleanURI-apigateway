5c588f7705e381276f4602a7d7d41b746a70db40
Stefan Haun <mail@tuxathome.de>
2022-07-21 14:47:19 +0200

    Refactor ResultTimeoutException to contain meaningful information

--- src/main/java/com/penguineering/cleanuri/apigateway/results/ResultTimeoutException.java
+++ src/main/java/com/penguineering/cleanuri/apigateway/results/ResultTimeoutException.java
@@ -1,10 +1,34 @@
 package com.penguineering.cleanuri.apigateway.results;
 
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.http.hateoas.JsonError;
+
+import java.util.Optional;
+
+/**
+ * This exception is thrown when an ExpectedResult is not provided within the configured timeout.
+ */
 public class ResultTimeoutException extends Exception{
-    public ResultTimeoutException() {
+    private final String correlationId;
+    public ResultTimeoutException(@Nullable String correlationId, String message) {
+        super(message);
+        this.correlationId = correlationId;
     }
 
-    public ResultTimeoutException(String message) {
-        super(message);
+    /**
+     * Get the correlation ID for the backend process.
+     *
+     * @return A correlation ID for debugging purposes.
+     */
+    public Optional<String> getCorrelationId() {
+        return correlationId == null ? Optional.empty() : Optional.of(correlationId);
+    }
+
+    /**
+     * Convert to JsonError and store the correlation ID as log ref.
+     * @return A JsonError for the exception.
+     */
+    public JsonError toJsonError() {
+        return new JsonError(this.getMessage()).logref(correlationId);
     }
 }
